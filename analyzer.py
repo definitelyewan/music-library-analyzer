5ddde4b45ec5084802ae5612ae771ed78212ce70
Ewan <ejones21@uoguelph.ca>
2024-05-05 15:59:55 -0400

    init

---
+++ analyzer.py
@@ -0,0 +1,129 @@
+from sys import platform
+import sys
+import subprocess
+from subprocess import PIPE
+import errno
+import shutil
+import os
+from colorama import Fore
+
+class Inform:
+
+    def __init__(self, step):
+        self.step = step
+    
+    def message(self, msg):
+        print(Fore.BLUE, "[" + str(self.step) + "] " + msg, end="")
+        print(Fore.RESET)
+        self.step += 1
+
+# run cmake commands
+def cmake_build(src, build, options):
+
+    # generate an array to pass to subprocess
+    command = ["cmake"]
+
+    if platform == "win32":
+        command.append("-DCMAKE_CXX_COMPILER=g++")
+        command.append("-DCMAKE_C_COMPILER=gcc")
+        command.append("-G")
+        command.append("MinGW Makefiles")
+
+    command.append("-S")
+    command.append(src)
+    command.append("-B")
+    
+    if platform == "linux" or platform == "linux2" or "darwin":
+        command.append("./" + build)
+    elif platform == "win32":
+        command.append(".\\" + build)
+
+
+    for option in options:
+        command.append(option)
+
+    # build with cmake
+    try:
+        subprocess.call(command)
+    
+    # error out
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            # program was not found
+            print(Fore.RED, f"[ERROR] : failed to run CMake! with command \"{command}\"")
+            quit()
+        else:
+            # program output
+            raise
+
+#compile c code
+def compile_code(project_name):
+    try:
+        if platform == "win32":
+            subprocess.call(["mingw32-make","-C",".\\build", project_name])
+        else:
+            subprocess.call(["make","-C","./build", project_name])
+
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            # program was not found
+            print(Fore.RED, "[ERROR] : failed to compile C code!")
+            quit()
+        else:
+            # program output
+            raise
+
+
+def write_mp3_file_extension_paths(directory, filename):
+    with open(filename, "wb") as f:
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith(".mp3"):
+                    f.write(os.path.join(root, file).encode() + b"\n")
+
+
+if __name__ == "__main__":
+
+    inform = Inform(1)
+
+    inform.message("Starting CMake build process")
+    
+    foldername = "build"
+    inform.message(f"Checking for {foldername} directory")
+    if not os.path.exists(foldername):
+        inform.message(f"Creating {foldername} directory")
+        os.mkdir(foldername)
+    else:
+        inform.message(f"Removing and recreating {foldername} directory")
+        shutil.rmtree(foldername)
+        os.mkdir(foldername)
+
+
+    cmake_build(".", "build", ["-DBUILD_SHARED_LIBS=ON", "-DDEDUB_ID3_SYMBOLS=ON"])
+    inform.message("Built C code with CMake")
+
+    inform.message("Compiling C code")
+    compile_code("toCSV")
+
+    inform.message("Creating a CSV file")
+
+    inform.message("Creating paths.txt file")
+    write_mp3_file_extension_paths(sys.argv[1], "build/paths.txt")
+
+    try:
+        if platform == "win32":
+            os.chdir("build")
+            subprocess.call(["toCSV.exe", "paths.txt"])
+            os.chdir("..")
+        else:
+            subprocess.call(["valgrind","--leak-check=full", "./build/toCSV", "build/paths.txt"])
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            # program was not found
+            print(Fore.RED, "[ERROR] : failed to run toCSV!")
+            quit()
+        else:
+            # program output
+            raise
+    inform.message("Generated CSV")
+    
---
+++ toCSV.c
@@ -0,0 +1,291 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <id3v1/id3v1.h>
+#include <id3v2/id3v2.h>
+#include <id3v2/id3v2Frame.h>
+#include <id3dev.h>
+
+void removeChar(char *str, char ch) {
+    char *src = str;
+    char *dst = str;
+    while (*src) {
+        if (*src != ch) {
+            *dst++ = *src;
+        }
+        src++;
+    }
+    *dst = '\0';
+}
+
+void replaceChar(char *str, char ch, char newCh) {
+    
+    if(str == NULL){
+        return;
+    }
+
+    char *src = str;
+    while (*src) {
+        if (*src == ch) {
+            *src = newCh;
+        }
+        src++;
+    }
+}
+
+void removeSpecialChars(char *str){
+    
+    if(str == NULL){
+        return;
+    }
+    
+    removeChar(str, ',');
+    removeChar(str, '\n');
+    removeChar(str, '\t');
+    removeChar(str, '\r');
+}
+
+int main(int argc, char *argv[]){
+
+    if(argc < 2){
+        printf("[ERROR] : no argv[1]\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *f = NULL;
+    FILE *csv = NULL;
+    size_t sz = 0;
+    size_t stringCount = 0;
+    char *fileContent = NULL;
+    char *index = NULL;
+    char **strings = NULL;
+
+    f = fopen(argv[1], "r");
+
+    if(f == NULL){
+        printf("[ERROR] : File not found!\n");
+        return EXIT_FAILURE;
+    }
+
+    // file size
+    fseek(f, 0, SEEK_END);
+    sz = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    // read file
+    fileContent = calloc(sz, sizeof(char));
+    fread(fileContent, sizeof(char), sz, f);
+
+    // create new strings
+    for(size_t i = 0; i < sz; i++){
+        if(fileContent[i] == '\n'){
+            fileContent[i] = '\0';
+            stringCount++;
+        }
+    }
+
+    // create a 2d array for strings
+    strings = calloc(stringCount, sizeof(char*));
+
+    index = fileContent;
+
+    size_t c = 0;
+    while(c < stringCount){
+        int len = 0;
+        len = strlen(index);
+
+        strings[c] = calloc(len + 1, sizeof(char));
+        strcpy(strings[c], index);
+        index += len + 1;
+        c++;
+    }
+
+    // create a file for a csv
+    csv = fopen("metadata.csv", "w");
+
+    if(csv == NULL){
+        printf("[ERROR] : Could not create metadata.csv\n");
+        
+        for(size_t i = 0; i < stringCount; i++){
+            free(strings[i]);
+        }
+        free(strings);
+        
+        return EXIT_FAILURE;
+    }
+
+
+    fwrite("ID3v1,ID3v2 ver,Title,Artist,Album Artist,Album,Year,Genre,Track,Composer,Disc,Lyrics,Comment,Pictures\n", sizeof(char), 104, csv);
+
+
+    for(size_t i = 0; i < stringCount; i++){
+
+        printf("[*] Reading %s for metadata\n", strings[i]);
+
+
+        ID3 *id3 = id3FromFile(strings[i]);
+
+        bool hasId3v1 = false;
+        int id3v2Version = 0;
+        char *title = NULL;
+        char *artist = NULL;
+        char *albumArtist = NULL;
+        char *album = NULL;
+        char *year = NULL;
+        char *genre = NULL;
+        char *track = NULL;
+        char *composer = NULL;
+        char *disc = NULL;
+        char *lyrics = NULL;
+        char *comment = NULL;
+        int pictures = 0;
+        char *line = NULL;
+        size_t memCount = 0;
+        Id3v2Frame *frame = NULL;
+        ListIter frames = {0};
+        
+
+
+        if(id3->id3v1 != NULL){
+            hasId3v1 = true;
+        }
+
+        if(id3->id3v2 != NULL){
+            id3v2Version = id3->id3v2->header->majorVersion;
+        }
+
+
+        title = id3ReadTitle(id3);
+        artist = id3ReadArtist(id3);
+        albumArtist = id3ReadAlbumArtist(id3);
+        album = id3ReadAlbum(id3);
+        year = id3ReadYear(id3);
+        genre = id3ReadGenre(id3);
+        track = id3ReadTrack(id3);
+        composer = id3ReadComposer(id3);
+        disc = id3ReadDisc(id3);
+        lyrics = id3ReadLyrics(id3);
+        comment = id3ReadComment(id3);
+        
+        frames = id3v2CreateFrameTraverser(id3->id3v2);
+        
+        while((frame = id3v2FrameTraverse(&frames)) != NULL){
+            if(memcmp(frame->header->id, "APIC", ID3V2_FRAME_ID_MAX_SIZE) == 0 || memcmp(frame->header->id, "PIC", ID3V2_FRAME_ID_MAX_SIZE) == 0){
+                pictures++;
+            }
+        }
+
+        removeSpecialChars(title);
+        removeSpecialChars(artist);
+        removeSpecialChars(albumArtist);
+        removeSpecialChars(album);
+        removeSpecialChars(year);
+
+        replaceChar(lyrics, ',', '/');
+        removeSpecialChars(genre);
+        removeSpecialChars(track);
+        removeSpecialChars(composer);
+        removeSpecialChars(disc);
+
+        replaceChar(lyrics, '\n', ' ');
+        removeSpecialChars(lyrics);
+        removeSpecialChars(comment);
+
+
+        memCount += snprintf(NULL, 0,
+                            "%s,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d\n",
+                            hasId3v1 ? "Yes" : "No",
+                            id3v2Version,
+                            title == NULL ? "" : title,
+                            artist == NULL ? "" : artist,
+                            albumArtist == NULL ? "" : albumArtist,
+                            album == NULL ? "" : album,
+                            year == NULL ? "" : year,
+                            genre == NULL ? "" : genre,
+                            track == NULL ? "" : track,
+                            composer == NULL ? "" : composer,
+                            disc == NULL ? "" : disc,
+                            lyrics == NULL ? "" : lyrics,
+                            comment == NULL ? "" : comment,
+                            pictures
+                            ); 
+                            
+        line = calloc(memCount + 1, sizeof(char));
+
+        snprintf(line, memCount + 1,
+                "%s,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%d\n",
+                hasId3v1 ? "Yes" : "No",
+                id3v2Version,
+                title == NULL ? "" : title,
+                artist == NULL ? "" : artist,
+                albumArtist == NULL ? "" : albumArtist,
+                album == NULL ? "" : album,
+                year == NULL ? "" : year,
+                genre == NULL ? "" : genre,
+                track == NULL ? "" : track,
+                composer == NULL ? "" : composer,
+                disc == NULL ? "" : disc,
+                lyrics == NULL ? "" : lyrics,
+                comment == NULL ? "" : comment,
+                pictures
+                );
+        
+        fwrite(line, sizeof(char), memCount, csv);
+
+
+        if(title != NULL){
+            free(title);
+        }
+        
+        if(artist != NULL){
+            free(artist);
+        }
+        
+        if(albumArtist != NULL){
+            free(albumArtist);
+        }
+        
+        if(album != NULL){
+            free(album);
+        }
+        
+        if(year != NULL){
+            free(year);
+        }
+        
+        if(genre != NULL){
+            free(genre);
+        }
+        
+        if(track != NULL){
+            free(track);
+        }
+        
+        if(composer != NULL){
+            free(composer);
+        }
+        
+        if(disc != NULL){
+            free(disc);
+        }
+        
+        if(lyrics != NULL){
+            free(lyrics);
+        }
+        
+        if(comment != NULL){
+            free(comment);
+        }
+
+
+        free(line);
+        id3Destroy(&id3);
+        free(strings[i]);
+    }
+    fclose(csv);
+    free(strings);
+    free(fileContent);
+    fclose(f);
+    return EXIT_SUCCESS;
+}
